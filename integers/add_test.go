7110963bcb451e0b06626d0ee10d91729591a654
Thang <thangnus@gmail.com>
2021-12-19 19:53:07 +1300

    feat: add example func in test

--- integers/add_test.go
+++ integers/add_test.go
@@ -1,10 +1,19 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	expected := 4
 	if sum := Add(2, 2); sum != 4 {
 		t.Errorf("Expected %q, got %v", expected, sum)
 	}
+}
+
+func ExampleAdd() {
+	sum := Add(3, 4)
+	fmt.Println(sum)
+	// Output: 7
 }

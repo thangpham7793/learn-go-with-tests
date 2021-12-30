1420368b161d58507314694a6bd136c28b757c68
Thang <thangnus@gmail.com>
2021-12-30 21:37:01 +1300

    refactor+fix(quiz_app)
    - Fix parse test cases not saving temp file content before parsing
    - Extract temp file creation to helper

--- quiz_app/parse_test.go
+++ quiz_app/parse_test.go
@@ -8,32 +8,39 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	t.Run("can parse properly formatted csv question set", func(t *testing.T) {
+
+	makeTempFile := func(t *testing.T, lines string) *os.File {
+		t.Helper()
+
 		dir, _ := os.Getwd()
 		f, _ := ioutil.TempFile(dir, "quiz")
-		f.WriteString("1+1,2")
+		f.WriteString(lines)
+		f.Close()
 
+		file, _ := os.Open(f.Name())
 		defer os.Remove(f.Name())
 
-		want := []Question{{prompt: "1+1", answer: "2"}}
-		got, _ := parse(f)
+		return file
+	}
 
-		if reflect.DeepEqual(got, want) {
+	t.Run("can parse properly formatted csv question set", func(t *testing.T) {
+		file := makeTempFile(t, "1+1,2")
+
+		want := []Question{{prompt: "1+1", answer: "2"}}
+		got, _ := parse(file)
+
+		if !reflect.DeepEqual(got, want) {
 			t.Errorf("Got %s want %s", got, want)
 		}
 	})
 
 	t.Run("can parse formatted csv question set with extra space", func(t *testing.T) {
-		dir, _ := os.Getwd()
-		f, _ := ioutil.TempFile(dir, "quiz")
-		f.WriteString("1+1, 2 ")
-
-		defer os.Remove(f.Name())
+		file := makeTempFile(t, "1+1,  2 ")
 
 		want := []Question{{prompt: "1+1", answer: "2"}}
-		got, _ := parse(f)
+		got, _ := parse(file)
 
-		if reflect.DeepEqual(got, want) {
+		if !reflect.DeepEqual(got, want) {
 			t.Errorf("Got %s want %s", got, want)
 		}
 	})
